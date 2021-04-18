952c8ad51866744bbdcd0ae4ef1fc77b16b1099f
shibuya-yudai <shibuya-yudai@users.noreply.github.com>
2021-04-18 15:41:25 +0000

    deploy: 460030d39279bd8ccb5d46e7fe262ed9027c366c

---
+++ _nuxt/static/1618760481/chooseSeat/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618760481",layout:"default",error:a,serverRendered:true,routePath:"\u002FchooseSeat",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
