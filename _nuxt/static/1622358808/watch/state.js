00ffa2c58dd4f27531e0e73ffdf6cb333a793ef2
shibuya-yudai <shibuya-yudai@users.noreply.github.com>
2021-05-31 10:57:02 +0000

    deploy: 900539eb2d9894caba9e510129c604f010a5ef5a

--- _nuxt/static/1622358808/watch/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fconnect\u002F_nuxt\u002Fstatic\u002F1622358808",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwatch",config:{_app:{basePath:"\u002Fconnect\u002F",assetsPath:"\u002Fconnect\u002F_nuxt\u002F",cdnURL:a}}}}(null));
