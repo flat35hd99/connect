038d5a97d0ace5b8464285a9db9bca99e543ed7f
澁谷悠大 <univer405@gmail.com>
2021-06-05 16:07:16 +0900

    reset

--- nuxt/pages/watch/index.vue
+++ nuxt/pages/watch/index.vue
@@ -6,7 +6,7 @@
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/GLIhyHy3ORI?controls=0"
+            src="https://youtu.be/00YaLKTF3mk&autoplay=1"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
